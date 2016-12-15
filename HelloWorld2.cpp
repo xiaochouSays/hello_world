3a6a94daf87572e06c0ae6a469aaad4c2cd76944
xiaochou <dunwenqiang@gmail.com>
2016-12-15 20:56:56 +0800

    add a blank file

---
+++ HelloWorld2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!ffHello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
