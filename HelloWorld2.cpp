1ae94c69562b68d273059842b76e032f72e1deff
xiaochou <dunwenqiang@gmail.com>
2016-12-15 21:30:27 +0800

    add Author

--- HelloWorld2.cpp
+++ HelloWorld2.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : HelloWorld2.cpp
-// Author      : 
-// Version     :
+// Author      : null
+// Version     : 
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
